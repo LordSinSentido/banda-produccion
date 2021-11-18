a76d8281e9c28548c70f63c1421dd74b01ba22ba
Lordy <57022857+LordSinSentido@users.noreply.github.com>
2021-11-18 08:51:55 -0600

    Se implementó la conexión con el puerto serial

---
+++ src/ConexionArduino.java
@@ -0,0 +1,26 @@
+
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+
+public class ConexionArduino {
+    private SerialPort puerto;
+    
+    public ConexionArduino() {
+        try {   // Se intenta realizar la conexión con el puerto serial, si se logra conectar ejecutará el siguiente código
+            puerto = new SerialPort("/dev/ttyACM0");   // Se especifica el puerto al que se trata de conectar
+            puerto.openPort();   // Se inicializa el puerto
+            puerto.setParams(SerialPort.BAUDRATE_19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);   // Se establecen los parámetros que tendrá la conexión
+        } catch (SerialPortException e) {   // En caso de que no se haya podido conectar, ejecutará el siguiente código
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void mandarMensaje(String mensaje) {
+        try {
+            puerto.writeString(mensaje);
+        } catch (SerialPortException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
