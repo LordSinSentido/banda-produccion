9d97c4ac6594bcdb1a3c7ccc0cce48e46b83f05d
LordSinSentido <misogu@live.com>
2021-11-20 23:22:05 -0600

    Se corrigió el error de los baudrate

--- src/ConexionArduino.java
+++ src/ConexionArduino.java
@@ -10,16 +10,16 @@ public class ConexionArduino {
         try {   // Se intenta realizar la conexión con el puerto serial, si se logra conectar ejecutará el siguiente código
             puerto = new SerialPort("/dev/ttyACM0");   // Se especifica el puerto al que se trata de conectar
             puerto.openPort();   // Se inicializa el puerto
-            puerto.setParams(SerialPort.BAUDRATE_19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);   // Se establecen los parámetros que tendrá la conexión
+            puerto.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);   // Se establecen los parámetros que tendrá la conexión
         } catch (SerialPortException e) {   // En caso de que no se haya podido conectar, ejecutará el siguiente código
             System.out.println(e.getMessage());
         }
     }
     
-    public void mandarMensaje(String mensaje) {
+    public void mandarMensaje(String valor) {
         try {
-            puerto.writeString(mensaje);
-            System.out.println(mensaje);
+            puerto.writeString(valor);
+            System.out.println(valor);
         } catch (SerialPortException e) {
             System.out.println(e.getMessage());
         }
