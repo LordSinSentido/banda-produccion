16d138b422cf4eb92b9e6718e7568d6b97f39532
LordSinSentido <misogu@live.com>
2021-11-20 23:21:41 -0600

    Se optimizó el código

--- conexion arduino/transmisor/transmisor.ino
+++ conexion arduino/transmisor/transmisor.ino
@@ -1,68 +1,72 @@
-#include <SPI.h>   // Librería SPI para comunicacion con el modulo
-#include <RH_NRF24.h>   // Librería RadioHead para controlar la antena nrf24
+#include <SPI.h>
+#include <RH_NRF24.h>
 
-RH_NRF24 nrf24;   // Se crea el objeto de la antena
+RH_NRF24 nrf24;
 
-bool listoParaTransmitir = true;   // Variable para corroborar que el dispositivo fue inicializado exitosamente
+bool listoParaTransmitir = true;
 
 void setup() {
-  // Iniciamos la comunicación con el puerto serie
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  // Inicializamos la conexión con la antena
-  if (!nrf24.init()) {   // Se verifica que la conexión se realice, si no, se ejecuta el siguiente código
-    Serial.println("No se pudo conectar con la antena");   // Se le informa al usuario mediante un mensaje
-    listoParaTransmitir = false;   // Se establece que el dispositivo no está listo para transmitir
+  if (!nrf24.init()) {
+    Serial.println("No se pudo conectar con la antena");
+    listoParaTransmitir = false;
   }
 
-  // Inicializamos el canal que se utilizará para comunicarnos con las demás antenas
-  if (!nrf24.setChannel(2)) {   // Se verifica que el canal se establezca, si no, se ejecuta el siguiente código
-    Serial.println("No se pudo establecer el canal de transmisión");   // Se le informa al usuario mediante un mensaje
-    listoParaTransmitir = false;   // Se establece que el dispositivo no está listo para transmitir
+  if (!nrf24.setChannel(2)) {
+    Serial.println("No se pudo establecer el canal de transmisión");
+    listoParaTransmitir = false;
   }
 
-  // Inicializamos los parámetros de enlace con las demás antenas
-  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) {   // Se verifica que los parámetros se establezcan, si no, se ejecuta el siguiente código
-    Serial.println("No se pudo configurar la transmisión");   // Se le informa al usuario mediante un mensaje
-    listoParaTransmitir = false;   // Se establece que el dispositivo no está listo para transmitir
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm)) {
+    Serial.println("No se pudo configurar la transmisión");
+    listoParaTransmitir = false;
   }
 
-  // Confirmamos que el dispositivo esté listo
-  if(listoParaTransmitir) {   // Verificamos que el dispositivo esté listo para transmitir, si es así, ejecutamos el siguiente código
-    Serial.println("Listo para transmitir...");   // Se le informa al usuario mediante un mensaje
-  }else{   // En caso contrario, se ejecuta el siguiente código
+  if(listoParaTransmitir) {
+    Serial.println("Listo para transmitir...");
+  }else{
     Serial.println("Hubo un problema, verifique la conexión y los datos y vuelva a intentar.");
   }
   
 }
 
 void loop() {
-  while(Serial.available() > 0) {   // Se comprueba que haya datos en el puerto serial
-    // No se ejecuta nada hasta que se rompa el ciclo
-  }
   
-  String estadoDeLaBanda = Serial.readString();
-  estadoDeLaBanda.trim();
+  if(Serial.available() > 0) {
+    int estado = Serial.parseInt();
 
-  if(estadoDeLaBanda == '\n') {   // Se comprueba que haya un salto de línea, si es así, se ejecuta el siguiente código
-    // No se ejecuta nada
-  }
-  
-  if(estadoDeLaBanda.equals("0")) {
-    uint8_t mensaje[] = "0";
-    Serial.print("Se envió: ");
-    Serial.println((char*)mensaje);
-    nrf24.send(mensaje, sizeof(mensaje));
-    nrf24.waitPacketSent();
-    
-  }
+    switch(estado) {
+      case 1:
+        digitalWrite(LED_BUILTIN, HIGH);
+        apagar();
+        delay(1000);
+        digitalWrite(LED_BUILTIN, LOW);
+        break;
 
-  if(estadoDeLaBanda.equals("1")) {
-    uint8_t mensaje[] = "1";
-    Serial.print("Se envió: ");
-    Serial.println((char*)mensaje);
-    nrf24.send(mensaje, sizeof(mensaje));
-    nrf24.waitPacketSent();
-    
+      case 2:
+        digitalWrite(LED_BUILTIN, HIGH);
+        encender();
+        delay(200);
+        digitalWrite(LED_BUILTIN, LOW);
+        break;
+    }
   }
+}
+
+void apagar() {
+  uint8_t mensaje[] = "0";
+  Serial.print("Se envió: ");
+  Serial.println((char*)mensaje);
+  nrf24.send(mensaje, sizeof(mensaje));
+  nrf24.waitPacketSent();
+}
+
+void encender() {
+  uint8_t mensaje[] = "1";
+  Serial.print("Se envió: ");
+  Serial.println((char*)mensaje);
+  nrf24.send(mensaje, sizeof(mensaje));
+  nrf24.waitPacketSent();
 }
